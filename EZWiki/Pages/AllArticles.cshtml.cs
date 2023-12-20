1bcebc68dd81ac1f6988cdbf69406329949bf066
Erik-Z <erikzhou7@gmail.com>
2023-12-19 21:27:26 -0500

    Added all articles page

---
+++ EZWiki/Pages/AllArticles.cshtml.cs
@@ -0,0 +1,34 @@
+using EZWiki.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace EZWiki.Pages
+{
+    public class AllArticlesModel : PageModel
+    {
+        public readonly ApplicationDbContext _context;
+        public const int _PageSize = 20;
+
+        public AllArticlesModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        [FromQuery]
+        public int PageNumber { get; set; } = 1;
+        public int TotalPages => (_context.Articles.Count() + _PageSize - 1) / _PageSize;
+        public IEnumerable<Article> Articles { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Articles = await _context.Articles
+                .AsNoTracking()
+                .OrderBy(a => a.Topic)
+                .Skip((PageNumber - 1) * _PageSize)
+                .Take(_PageSize)
+                .ToArrayAsync();
+                
+        }
+    }
+}
